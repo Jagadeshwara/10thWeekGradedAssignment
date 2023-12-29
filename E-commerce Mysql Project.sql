701c7cf1e6ac4f7e6709ff23d6193a8447b9838f
Jagadeshwara chary vadla <98221668+Jagadeshwara@users.noreply.github.com>
2023-12-29 21:16:26 +0530

    Update E-commerce Mysql Project.sql

--- E-commerce Mysql Project.sql
+++ E-commerce Mysql Project.sql
@@ -211,3 +211,13 @@ END //
 DELIMITER ;
 
 
+-- 2nd question answer
+create database greatlearning;
+use greatlearning;
+
+create table employee(
+Id int primary key,
+Name varchar(50) not null,
+Email_id varchar(50) not null,
+Phone_Number bigint);
+
